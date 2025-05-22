67ff76dae7f1e530bfa1bc1fddfc0b5364c30083
lynnlee0522 <lilin@arcblock.io>
2025-05-22 12:02:17 +0800

    update wpm widget

--- app/src/display/widgets/battery_status.c
+++ app/src/display/widgets/battery_status.c
@@ -7,7 +7,7 @@
 #include <zephyr/kernel.h>
 
 #include <zephyr/logging/log.h>
-LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+// LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include <zmk/battery.h>
 #include <zmk/display.h>
--- app/src/display/widgets/wpm_status.c
+++ app/src/display/widgets/wpm_status.c
@@ -24,14 +24,45 @@ struct wpm_status_state wpm_status_get_state(const zmk_event_t *eh) {
     return (struct wpm_status_state){.wpm = zmk_wpm_get_state()};
 };
 
-void set_wpm_symbol(lv_obj_t *label, struct wpm_status_state state) {
-    char text[4] = {};
+void set_wpm_symbol(lv_obj_t *arc, struct wpm_status_state state) {
+    // 清除cont的所有子对象
+    lv_obj_clean(arc);
 
-    LOG_DBG("WPM changed to %i", state.wpm);
-    snprintf(text, sizeof(text), "%i", state.wpm);
+    // lv_obj_set_style_border_width(cont, 0, 0);       // 移除边框
+    // lv_obj_set_style_bg_opa(cont, LV_OPA_TRANSP, 0); // 设置背景为透明
 
+    char text[8] = {};
+    // 创建圆弧控件
+    // lv_obj_t *arc = lv_arc_create(cont);
+    lv_obj_set_size(arc, 60, 60);
+    lv_arc_set_rotation(arc, 135);
+    lv_arc_set_bg_angles(arc, 0, 270);
+    lv_arc_set_range(arc, 0, 200);
+
+    lv_obj_set_style_arc_width(arc, 5, LV_PART_MAIN);      // 背景圆弧宽度
+    lv_obj_set_style_arc_width(arc, 5, LV_PART_INDICATOR); // 指示器圆弧宽度
+
+    // 设置指示器颜色为黄色
+    lv_obj_set_style_arc_color(arc, lv_color_make(255, 255, 255), LV_PART_MAIN);
+    lv_obj_set_style_arc_color(arc, lv_color_make(255, 215, 0), LV_PART_INDICATOR);
+    lv_obj_set_style_bg_color(arc, lv_color_make(255, 215, 0), LV_PART_KNOB);
+    lv_obj_set_style_arc_color(arc, lv_color_make(255, 215, 0), LV_PART_KNOB);
+
+    int arc_value = (state.wpm > 200) ? 200 : state.wpm;
+    lv_arc_set_value(arc, arc_value);
+    lv_obj_center(arc);
+
+    // 创建标签显示WPM值
+    lv_obj_t *label = lv_label_create(arc);
+    snprintf(text, sizeof(text), "%d\n%s", state.wpm, "wpm");
     lv_label_set_text(label, text);
-    lv_obj_align(label, LV_ALIGN_BOTTOM_RIGHT, 0, 0);
+
+    // 设置标签样式
+    lv_obj_set_style_text_font(label, &lv_font_montserrat_12, 0);
+    lv_obj_set_style_text_color(label, lv_color_make(255, 255, 255), 0);
+
+    // 将标签居中显示在圆弧中心
+    lv_obj_center(label);
 }
 
 void wpm_status_update_cb(struct wpm_status_state state) {
@@ -44,11 +75,10 @@ ZMK_DISPLAY_WIDGET_LISTENER(widget_wpm_status, struct wpm_status_state, wpm_stat
 ZMK_SUBSCRIPTION(widget_wpm_status, zmk_wpm_state_changed);
 
 int zmk_widget_wpm_status_init(struct zmk_widget_wpm_status *widget, lv_obj_t *parent) {
-    widget->obj = lv_label_create(parent);
-    lv_obj_align(widget->obj, LV_ALIGN_RIGHT_MID, 0, 0);
+    widget->obj = lv_arc_create(parent);
+    lv_obj_set_style_text_align(widget->obj, LV_TEXT_ALIGN_CENTER, 0);
 
     sys_slist_append(&widgets, &widget->node);
-
     widget_wpm_status_init();
     return 0;
 }
